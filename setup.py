132c200d75273d8e0589e47ee71305b4115f6405
Basil Huber <basil.huber@gmail.com>
2022-04-17 16:13:37 +0200

    bumps version to v0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(name='PyDriveBrowser',
-      version='0.1.1',
+      version='0.1.2',
       description='Command line file Browser for Google Drive',
       long_description=long_description,
       long_description_content_type='text/markdown',
